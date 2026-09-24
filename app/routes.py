760d26eb312203c3bc4802a3c86e7756e4e7ad11
ing327 <tadolfo_va@hotmail.com>
2026-09-23 22:01:31 -0500

    retoques

--- app/routes.py
+++ app/routes.py
@@ -1,18 +1,12 @@
-from flask import Blueprint, render_template, request
-
+from flask import Blueprint, render_template, request, redirect, url_for
 from .models import Producto, Categoria
-
 
 main = Blueprint("main", __name__)
 
 
 @main.route("/")
 def index():
-
-    categoria_id = request.args.get(
-        "categoria",
-        type=int
-    )
+    categoria_id = request.args.get("categoria", type=int)
 
     if categoria_id:
         productos = Producto.query.filter_by(
@@ -33,9 +27,18 @@ def index():
     )
 
 
+@main.route("/buscar")
+def buscar():
+    sku = request.args.get("sku", "").strip()
+
+    if not sku:
+        return redirect(url_for("main.index"))
+
+    return redirect(url_for("main.detalle", sku=sku))
+
+
 @main.route("/producto/<sku>")
 def detalle(sku):
-
     producto = Producto.query.filter_by(
         sku=sku
     ).first_or_404()
@@ -48,7 +51,6 @@ def detalle(sku):
 
 @main.route("/categorias")
 def categorias():
-
     categorias = Categoria.query.order_by(
         Categoria.nombre
     ).all()
